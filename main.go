43f175422c0f57ddd8e1d22ec4e9f04f9b054d31
aaaasmile <testhaus@gmx.at>
2021-03-13 18:00:28 +0100

    add doc

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+func main() {
+	fmt.Println("Rename files")
+	dirToScan := `D:\scratch\go-lang\rename-file\example2`
+	//dirToScan := `G:\02`
+	files, err := ioutil.ReadDir(dirToScan)
+	if err != nil {
+		panic(err)
+	}
+	// The mp3 filename format for Yx5300 Chip is:
+	// 001xxx.mp3
+	// use the inial filename instead of xxx
+	renum := regexp.MustCompile("[0-9]+")
+	for ix, ffInfo := range files {
+		if !ffInfo.IsDir() {
+			fmt.Println("File: ", ix, ffInfo.Name())
+			src := filepath.Join(dirToScan, ffInfo.Name())
+
+			part := ffInfo.Name()[:3]
+			arr := renum.FindAllString(part, -1)
+			if len(arr) == 1 && len(arr[0]) == 3 {
+				fmt.Println("already processed item ", arr[0])
+				part = ffInfo.Name()[3:6]
+			}
+			partascii := StringToAsciiBytes(part) // avoid accent characters trouble in filename
+			fmt.Println("ascii: ", partascii)
+
+			part = strings.ReplaceAll(string(partascii), " ", "x")
+			dst := fmt.Sprintf("%03d%s.mp3", ix+1, part)
+			dst = filepath.Join(dirToScan, dst)
+			err = os.Rename(src, dst)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("Rename : ", src, dst)
+		}
+	}
+
+}
+
+func StringToAsciiBytes(s string) []byte {
+	t := make([]byte, utf8.RuneCountInString(s))
+	i := 0
+	for _, r := range s {
+		t[i] = byte(r)
+		if t[i] < 48 ||
+			((t[i] > 57) && (t[i] < 65)) ||
+			((t[i] > 90) && (t[i] < 97)) ||
+			(t[i] > 122) {
+			t[i] = 120 // if it is not a valid alpha numeric character (i.e. 'è'), then is x
+		}
+		i++
+	}
+	return t
+}
---
+++ passaggi.md
@@ -0,0 +1,23 @@
+# Rename-File
+Questo piccolo programma mi serve per rinominare i files mp3 per poi metterli 
+su una sd card e lanciarli usando il player Yx5300 e ESP8266.
+
+## Formato dei files
+Il formato del file è mp3 con un nome a 6 caratteri, dove i primi 3 sono un numero
+compreso tra 001 e 999.
+
+## Creazione della Lista
+Il programma https://github.com/aaaasmile/MP3-YX5300 esegue tutti i files messi all'interno
+di un folder, per esempio il folder 01, in una lista casuale senza ripetizioni.
+Quindi è importante avere i files mp3 con il nome giusto per esere lanciati.
+Per creare una lista copio i files mp3 presi da itunes manualmente nella directory ./folder_01
+Qui i nomi dei files sono i più disparati. 
+Ora lancio questo programma Rename-file con go run main.go e la lista dei files dovrebbe essere
+pronta. 
+Ora bisogna copiare il contenuto di folder_01 nella sd-card del player e modificare il codice di 
+MP3-YX5300. 
+Come mai bisogna modificare il codice? Perché il comando seriale AT del player che teoriacamnte
+dovrebbe fornire il numero dei files nel folder 01 della SD-card non sono riuscito a farlo andare.
+Il valore che legge è sempre zero.
+Siccome la lista dei files mp3 non è che viene attualizzata spesso, quando succede, basta ricompilare
+il software con il valore corretto.
